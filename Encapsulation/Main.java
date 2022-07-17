7081536f431ea1fd688720ff4e674a5a333bd5a7
Thangtran1100 <thangtrn117@gmail.com>
2022-07-16 17:20:04 -0700

    update

--- Encapsulation/Main.java
+++ Encapsulation/Main.java
@@ -4,18 +4,21 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Player player = new Player();
+        // Player player = new Player();
 
-        player.name = "Summer";
-        player.health = 100;
-        player.weapon = "Gun";
+        // player.name = "Summer";
+        // player.health = 100;
+        // player.weapon = "Gun";
 
-        int damage = 10;
-        player.loseHealth(damage);
-        System.out.println("Remaining health = " + player.healthRemaining());
+        // int damage = 10;
+        // player.loseHealth(damage);
+        // System.out.println("Remaining health = " + player.healthRemaining());
 
-        damage = 90;
-        player.loseHealth(damage);
-        System.out.println("Remaining health = " + player.healthRemaining());
+        // damage = 90;
+        // player.loseHealth(damage);
+        // System.out.println("Remaining health = " + player.healthRemaining());
+
+        EnhancedPlayer player = new EnhancedPlayer("Summer", 50, "Sword");
+        System.out.println("Initial health is " + player.getHealth());
     }
 }
