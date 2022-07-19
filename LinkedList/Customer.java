99dfc15a1a08ff62815b101e8290f3c180d511de
Thangtran1100 <thangtrn117@gmail.com>
2022-07-18 22:59:05 -0700

    a

---
+++ LinkedList/Customer.java
@@ -0,0 +1,22 @@
+package LinkedList;
+
+public class Customer {
+    private String name;
+    private double balance;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
